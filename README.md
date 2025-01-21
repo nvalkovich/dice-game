fd01db5e35cb68eb8375ecdb02f0e3c098d87f1e
nvalkovich <anastasiya.valkovich@mail.ru>
2025-01-21 15:57:07 +0300

    chore: initialize project with TypeScript, ESLint and Prettier

--- README.md
+++
@@ -1,2 +0,0 @@
-# dice-game
-A console script that implements a generalized non-transitive dice game (with the supports of arbitrary values on the dice)
---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import eslint from '@eslint/js';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+    {
+        files: ['**/*.ts'],
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+            },
+            globals: {
+                crypto: 'readonly',
+                process: 'readonly',
+                console: 'readonly',
+            },
+        },
+        plugins: {
+            '@typescript-eslint': tsPlugin,
+        },
+        rules: {
+            'no-unused-vars': 'error',
+            'no-undef': ['error', { typeof: true }],
+            'prefer-const': 'error',
+            ...eslint.configs.recommended.rules,
+            ...tsPlugin.configs.recommended.rules,
+        },
+        settings: {
+            node: true,
+        },
+        ignores: ['dist', 'node_modules'],
+    },
+];
